f055ab79cdfaeac60b2b782c83ffc91f00894e97
Chris Joakim <christopher.joakim@gmail.com>
2020-02-24 18:19:26 -0500

    moving to newer Azure.Storage.Blobs SDK

--- blobs/Program.cs
+++ blobs/Program.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿
+using Azure.Storage;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System;
 using System.Collections;
-using System.Threading;
+using System.IO;
+using System.Threading.Tasks;
+
 
 // https://docs.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-dotnet-legacy
 
@@ -12,18 +18,14 @@ namespace blobs
         {
             Console.WriteLine("starting");
 
-            if (args.Length == 0)
-            {
-                Console.WriteLine("no command-line args");
-            }
-            else
-            {
-                foreach (string arg in args)
-                {
-                    Console.WriteLine("arg: " + arg);
-                }
-            }
-                
+            string containerName = blobContainer();
+            Console.WriteLine("containerName: {0}", containerName);
+
+            string connectionString = 
+                Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+            //BlobContainerClient containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
             foreach (DictionaryEntry de in Environment.GetEnvironmentVariables())
             {
                 Console.WriteLine("{0} = {1}", de.Key, de.Value);
@@ -33,19 +35,27 @@ namespace blobs
             System.Threading.Thread.Sleep(10000);
             Console.WriteLine("exiting");
         }
+
+        static string blobContainer()
+        {
+            char[] charSeparators = new char[] { '/' };
+            string blobPath = Environment.GetEnvironmentVariable("BLOB_PATH");
+            return blobPath.Split(charSeparators)[1];
+        }
+
     }
 }
 
 //{
-//  "Id": "JTJmc2ltdWxhdGlvbnMlMmZwb3N0YWxfY29kZXNfY3QuY3N2",
-//  "Name": "postal_codes_ct.csv",
-//  "DisplayName": "postal_codes_ct.csv",
-//  "Path": "/simulations/postal_codes_ct.csv",
-//  "LastModified": "2020-02-24T21:15:12Z",
-//  "Size": 24394,
+//  "Id": "JTJmc2ltdWxhdGlvbnMlMmZwb3N0YWxfY29kZXNfbmMuY3N2",
+//  "Name": "postal_codes_nc.csv",
+//  "DisplayName": "postal_codes_nc.csv",
+//  "Path": "/simulations/postal_codes_nc.csv",
+//  "LastModified": "2020-02-24T22:13:23Z",
+//  "Size": 61273,
 //  "MediaType": "text/csv",
 //  "IsFolder": false,
-//  "ETag": "\"0x8D7B96EA3067BB7\"",
-//  "FileLocator": "JTJmc2ltdWxhdGlvbnMlMmZwb3N0YWxfY29kZXNfY3QuY3N2",
+//  "ETag": "\"0x8D7B976C3E20740\"",
+//  "FileLocator": "JTJmc2ltdWxhdGlvbnMlMmZwb3N0YWxfY29kZXNfbmMuY3N2",
 //  "LastModifiedBy": null
 //}
