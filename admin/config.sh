c3124fd357b3aafabc737670e96473aeef8698e9
Chris Joakim <christopher.joakim@gmail.com>
2020-02-26 16:11:05 -0500

    Implemented the python-based ACI deletion process

---
+++ admin/config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script is sourced by the other scripts in this directory.
+# Chris Joakim, Microsoft, 2020/02/26
+
+export resource_group=cjoakim-logic
+export container_list_file=tmp/container_list.json
--- admin/gen_aci_delete_script.sh
+++ admin/gen_aci_delete_script.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# Chris Joakim, Microsoft, 2020/02/26
+# https://docs.microsoft.com/en-us/cli/azure/container?view=azure-cli-latest#az-container-delete
 
-echo 'done'
+source ./config.sh
 
+rm $container_list_file
+rm aci_deletes.sh
+
+echo 'resource_group: '$resource_group
+
+#az login
+
+az container list --subscription $AZURE_SUBSCRIPTION_ID --resource-group $resource_group > $container_list_file
+
+python main.py gen_aci_delete_script $resource_group $container_list_file
+
+chmod 744 aci_deletes.sh
+
+echo 'displaying the generated aci_deletes.sh script below:'
+echo ''
+cat aci_deletes.sh
+echo ''
