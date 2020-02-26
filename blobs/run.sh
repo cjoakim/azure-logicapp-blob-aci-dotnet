abcf7f33882aacace0ef827078c740502e0ddebf
Chris Joakim <christopher.joakim@gmail.com>
2020-02-26 11:40:07 -0500

    logic app working concurrently, captured code.json

--- blobs/run.sh
+++ blobs/run.sh
@@ -4,16 +4,17 @@
 # Chris Joakim, Microsoft, 2020/02/26
 
 # export RUNTYPE=env
+# export RUNTYPE=logic_app_process_blob
 # export RUNTYPE=populate_storage_blobs
-export RUNTYPE=logic_app_process_blob
+export RUNTYPE=populate_storage_blobs
 
-export POPULATE_STORAGE_RANDOM_PCT=5
+export POPULATE_STORAGE_RANDOM_PCT=2
 export POPULATE_STORAGE_DELAY=3
 export TARGET_BLOB_CONTAINER=processed
 
 # these are used to simulate the processing of a blob in the Logic App
-export SOURCE_BLOB_NAME=benjamin_hebert_1582653235.csv
-export SOURCE_BLOB_PATH=/simulations/benjamin_hebert_1582653235.csv
+export SOURCE_BLOB_NAME=cjoakim.csv
+export SOURCE_BLOB_PATH=/simulations/cjoakim.csv
 
 # AZURE_STORAGE_CONNECTION_STRING is already an env var on my workstatiob.
 
