0408454baa11f76a845df1f2105c4261cee95382
Chris Joakim <christopher.joakim@gmail.com>
2020-05-05 16:02:09 -0400

    Added automation/create-service-principal.sh

---
+++ automation/create-service-principal.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Bash script to execute the Azure CLI (az) to create an Azure Service Principal.
+# Chris Joakim, Microsoft, 2020/05/05
+
+az ad sp create-for-rbac --help
+
+az ad sp create-for-rbac --name chjoakim20200418
+
+# Output:
+# Changing "chjoakim20200418" to a valid URI of "http://chjoakim20200418", which is the required format used for service principal names
+# Creating a role assignment under the scope of "/subscriptions/61761119-d249-4507-90c6-a16517e1874c"
+#   Retrying role assignment creation: 1/36
+# {
+#   "appId": "fffcc82e-03a5-4aa2-...",
+#   "displayName": "chjoakim20200418",
+#   "name": "http://chjoakim20200418",
+#   "password": "8a7aa712-ff89-4172-...",
+#   "tenant": "72f988bf-86f1-41af-..."
+# }
